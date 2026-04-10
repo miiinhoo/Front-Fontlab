12a8a6320657f0d4307ce58345f9fa1c47d87882
miiinhoo <alsgh4454@naver.com>
2026-04-10 22:41:33 +0900

    add : mainpage추가

---
+++ src/arrays/FontArrays.tsx
@@ -0,0 +1,26 @@
+export const FontMap = {
+  serif: [
+    "Playfair Display",
+    "Merriweather",
+    "Lora",
+    "Libre Baskerville",
+    "Cormorant",
+    "Georgia"
+  ],
+  sans: [
+    "Inter",
+    "Roboto",
+    "Open Sans",
+    "Lato",
+    "Montserrat",
+    "Poppins"
+  ],
+  display: [
+    "Pacifico",
+    "Bebas Neue",
+    "Abril Fatface",
+    "Lobster",
+    "Anton",
+    "Righteous"
+  ]
+};
